4c10166a37db9c937008ff1fbc0d63e74a16b7d0
Justin Gallagher <justingallagher512@gmail.com>
2023-03-06 16:52:51 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,40 @@
 # log-mon
 Monitor log file for new events and sent directly to discord channel
+
+# C3PO
+
+Sample discord bot in Python with a few events.
+
+
+## Run Locally
+
+Clone the project
+
+```bash
+  git clone https://link-to-project
+```
+
+Go to the project directory
+
+```bash
+  cd my-project
+```
+
+Create .env file in the project directory with the following
+
+```bash
+  # .env
+    DISCORD_TOKEN=TOKENHERE
+    DISCORD_GUILD=SERVERID HERE
+```
+
+Install dependencies
+
+```bash
+  pip install python-dotenv  
+  pip install discord.py
+```
+
+Start the server
+ 
+
---
+++ main.py
@@ -0,0 +1,47 @@
+# Main.py
+import discord
+import os
+import asyncio
+
+
+from dotenv import load_dotenv
+
+load_dotenv()
+TOKEN = os.getenv('DISCORD_TOKEN')
+GUILD = os.getenv('DISCORD_GUILD')
+CHANNEL_ID = 1081735735893311498 #<-- Right click the channel you would like reporting in to get this
+LOG_FILE = "/var/log/auth.log"
+client = discord.Client(intents=discord.Intents.default())
+
+# Words to search for in the log file
+SEARCH_WORDS = ['cinnamon', 'authentication', 'failure']
+
+# Keep track of the last position in the file
+last_position = os.path.getsize(LOG_FILE)
+
+# Check for new lines containing the search words in the log file
+async def check_log_file():
+    global last_position
+    while True:
+        with open(LOG_FILE, 'r') as file:
+            file.seek(last_position)
+            line = file.readline().strip()
+            while line:
+                if any(word in line.lower() for word in SEARCH_WORDS):
+                    await send_log_message(line)
+                line = file.readline().strip()
+            last_position = file.tell()
+        await asyncio.sleep(1)
+
+# Send the log message to the designated channel
+async def send_log_message(message):
+    channel = client.get_channel(CHANNEL_ID)
+    await channel.send(message)
+
+# Discord bot events
+@client.event
+async def on_ready():
+    print(f'{client.user} is online!')
+    await check_log_file()
+
+client.run(TOKEN)
